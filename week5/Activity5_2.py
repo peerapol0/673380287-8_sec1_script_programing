869f28e47624c19eb0ee62346fbe52c7da64c6dd
peeky001 <peerapol123456789k@gmail.com>
2026-07-22 16:08:59 +0700

    Update Activity5_2.py

--- week5/Activity5_2.py
+++ week5/Activity5_2.py
@@ -5,15 +5,15 @@ words = sentence.split()
 
 word_counts = {}
 
-for word in words:
-    cleaned_word = word.strip('.,!?"')
-    if cleaned_word:
-        if cleaned_word in word_counts:
-            word_counts[cleaned_word] += 1
+for w in words:
+    word = w.strip('.,!?"')
+    if word:
+        if word in word_counts:
+            word_counts[word] += 1
         else:
-            word_counts[cleaned_word] = 1
+            word_counts[word] = 1
 
-print(word_counts)
+
 print("Word counts:")
 for word, count in word_counts.items():
     print(f"{word}: {count}")
